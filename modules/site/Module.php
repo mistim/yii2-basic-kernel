a5912dafe2781c219be0db3369f5b41ce3888214
mistim <m.mistim@gmail.com>
2016-06-08 16:05:46 +0300

    - namespaces;

---
+++ modules/site/Module.php
@@ -0,0 +1,14 @@
+<?php
+namespace mistim\modules\site;
+
+use Yii;
+
+/**
+ * Class Module
+ * @package mistim\modules\site
+ */
+class Module extends \yii\base\Module
+{
+    /** @var string $defaultRoute */
+    public $defaultRoute = 'main';
+}
