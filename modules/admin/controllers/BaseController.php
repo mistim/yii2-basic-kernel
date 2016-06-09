db56fb8c59bbf37603e5b1a1b4056c56adac9486
mistim <m.mistim@gmail.com>
2016-06-09 10:51:05 +0300

    - find views path;

--- modules/admin/controllers/BaseController.php
+++ modules/admin/controllers/BaseController.php
@@ -3,6 +3,7 @@ namespace mistim\modules\admin\controllers;
 
 use Yii;
 use yii\filters\VerbFilter;
+use yii\helpers\FileHelper;
 use yii\web\Controller;
 use mistim\modules\rbac\components\AccessControl;
 
@@ -30,10 +31,51 @@ class BaseController extends Controller
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         parent::init();
 
         Yii::$app->getView()->params['sidebar-collapse'] = $this->sidebarCollapse;
+    }
+
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     * @throws \yii\web\BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+        $this->findViewDir($action);
+
+        return parent::beforeAction($action);
+    }
+
+    /**
+     * @param $action
+     */
+    protected function findViewDir($action)
+    {
+        $templateVendorViewPath = '@vendor/mistim/{vendor}/modules/admin/views/' . $action->controller->id;
+
+        if (file_exists(FileHelper::normalizePath(Yii::getAlias('@app/modules/admin/views/' . $action->controller->id)))) {
+            $this->setViewPath('@app/modules/admin/views/' . $action->controller->id);
+        } elseif (file_exists(FileHelper::normalizePath(Yii::getAlias('@app/views/' . $action->controller->id)))) {
+            $this->setViewPath('@app/views/' . $action->controller->id);
+        } else {
+            foreach (Yii::$app->extensions as $key => $item) {
+                if (substr_count($key, 'mistim/') > 0) {
+                    $vendorName = explode('/', $item['name'])[1];
+                    $viewPath = FileHelper::normalizePath(Yii::getAlias(str_replace('{vendor}', $vendorName,$templateVendorViewPath)));
+
+                    if (file_exists($viewPath)) {
+                        $this->setViewPath($viewPath);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
--- modules/site/controllers/BaseController.php
+++ modules/site/controllers/BaseController.php
@@ -1,8 +1,10 @@
 <?php
 namespace mistim\modules\site\controllers;
 
+use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
+use yii\helpers\FileHelper;
 use yii\web\Controller;
 
 /**
@@ -45,5 +47,43 @@ class BaseController extends Controller
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
+    }
+
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     * @throws \yii\web\BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+        $this->findViewDir($action);
+
+        return parent::beforeAction($action);
+    }
+
+    /**
+     * @param $action
+     */
+    protected function findViewDir($action)
+    {
+        $templateVendorViewPath = '@vendor/mistim/{vendor}/modules/admin/views/' . $action->controller->id;
+
+        if (file_exists(FileHelper::normalizePath(Yii::getAlias('@app/modules/admin/views/' . $action->controller->id)))) {
+            $this->setViewPath('@app/modules/admin/views/' . $action->controller->id);
+        } elseif (file_exists(FileHelper::normalizePath(Yii::getAlias('@app/views/' . $action->controller->id)))) {
+            $this->setViewPath('@app/views/' . $action->controller->id);
+        } else {
+            foreach (Yii::$app->extensions as $key => $item) {
+                if (substr_count($key, 'mistim/') > 0) {
+                    $vendorName = explode('/', $item['name'])[1];
+                    $viewPath = FileHelper::normalizePath(Yii::getAlias(str_replace('{vendor}', $vendorName,$templateVendorViewPath)));
+
+                    if (file_exists($viewPath)) {
+                        $this->setViewPath($viewPath);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
